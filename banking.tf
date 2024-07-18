d9ec63a7ec0772deb99d55ad1789eed3ea7d6c78
shalakashalaka <shalakaingle12@gmail.com>
2024-07-18 21:06:36 +0530

    Update banking.tf
    updating image id

--- banking.tf
+++ banking.tf
@@ -126,7 +126,7 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
+ ami = "ami-04a81a99f5ec58529"
  instance_type = "t2.micro"
  availability_zone = "us-east-1c"
  key_name = "Ubuntu Instance key"
