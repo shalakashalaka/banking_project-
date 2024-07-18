d0335a985fa9f68f15bf98b45fb0a9bce09c2cd3
shalakashalaka <shalakaingle12@gmail.com>
2024-07-18 21:04:39 +0530

    Update banking.tf
    availabilty zone

--- banking.tf
+++ banking.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 # Creating a VPC
 resource "aws_vpc" "proj-vpc" {
@@ -46,7 +46,7 @@ resource "aws_route_table" "proj-rt" {
 resource "aws_subnet" "proj-subnet" {
  vpc_id = aws_vpc.proj-vpc.id
  cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
+ availability_zone = "us-east-1c"
  tags = {
  Name = "subnet1"
  }
@@ -126,7 +126,7 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
- ami = "ami-04a81a99f5ec58529"
+ ami = "ami-0ef82eeba2c7a0eeb"
  instance_type = "t2.micro"
  availability_zone = "us-east-1c"
  key_name = "Ubuntu Instance key"
